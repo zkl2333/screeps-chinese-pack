d33faad8abd68173b960b808f8bb4994bd173f45
zkl <i@zkl2333.com>
2026-08-17 17:23:43 +0800

    release: 🏹 发布 1.9.1 版本

--- banner.js
+++ banner.js
@@ -8,7 +8,7 @@ export default `
 // ==UserScript==
 // @name         screeps-chinese-pack
 // @namespace    https://github.com/zkl2333/screeps-chinese-pack
-// @version      1.9.0
+// @version      1.9.1
 // @description  用于汉化 screeps.com 网站的油猴脚本
 // @author       zkl2333
 // @homepageURL  https://github.com/zkl2333/screeps-chinese-pack
